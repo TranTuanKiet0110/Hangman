53edcfc57a6d57a51d00c611ea7b520747f7645e
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2023-08-11 09:46:45 +0700

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by Kiet Tran Tuan on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
