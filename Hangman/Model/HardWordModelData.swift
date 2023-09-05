d7fc95d9acc7dc845a91962e38c3f47d99cf83e9
TranTuanKiet0110 <74531016+TranTuanKiet0110@users.noreply.github.com>
2023-09-05 21:19:05 +0700

    Update HardWordModelData.swift

--- Hangman/Model/HardWordModelData.swift
+++ Hangman/Model/HardWordModelData.swift
@@ -1,9 +1,14 @@
-//
-//  HardWordModelData.swift
-//  Hangman
-//
-//  Created by Kiet Tran Tuan on 25/08/2023.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 2
+  Author: Tran Tuan Kiet
+  ID: 3879300
+  Created  date: 25/08/2023
+  Last modified: 05/09/2023
+  Acknowledgement: 
+*/
 
 import Foundation
 
