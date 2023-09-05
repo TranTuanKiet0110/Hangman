6f38d7473ab0068a331c311d1fb1a2c22710b3de
TranTuanKiet0110 <74531016+TranTuanKiet0110@users.noreply.github.com>
2023-09-05 21:16:47 +0700

    Update LeaderboardView.swift

--- Hangman/Views/LeaderboardView.swift
+++ Hangman/Views/LeaderboardView.swift
@@ -1,9 +1,14 @@
-//
-//  LeaderboardView.swift
-//  Hangman
-//
-//  Created by Kiet Tran Tuan on 13/08/2023.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 2
+  Author: Tran Tuan Kiet
+  ID: 3879300
+  Created  date: 13/08/2023
+  Last modified: 05/09/2023
+  Acknowledgement: 
+*/
 
 import SwiftUI
 
